cfd0c43de7f06ac3e0a8d3a8e6768dfc146126d6
CStank89 <C.Stank89@gmail.com>
2020-08-18 10:15:12 -0400

    trying to work on figuring out why my arry filter on line 16 isnt working

--- index.js
+++ index.js
@@ -6,30 +6,45 @@ console.log('its working');
 
 /* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
 
-(a) Home Team name for 2014 world cup final
-(b) Away Team name for 2014 world cup final
-(c) Home Team goals for 2014 world cup final
-(d) Away Team goals for 2014 world cup final
-(e) Winner of 2014 world cup final */
+(a) Home Team name for 2014 world cup final*/
+console.log('The home team for the 2014 World Cup Final was ' + fifaData.filter(home => {return home.Year === 2014 && home.Stage === 'Final'})[0]['Home Team Name']);
+
+/*(b) Away Team name for 2014 world cup final*/
+console.log('The home team for the 2014 World Cup Final was ' + fifaData.filter(home => {return home.Year === 2014 && home.Stage === 'Final'})[0]["Away Team Name"]);
+
+/*(c) Home Team goals for 2014 world cup final*/
+console.log('The home team for the 2014 World Cup Final was ' + fifaData.filter(home => {return homeGoals.Year === 2014 && home.Stage === 'Final'})[0]["Home Team Name"]);
+
+/*(d) Away Team goals for 2014 world cup final*/
+
+console.log('The home team for the 2014 World Cup Final was ' + fifaData.filter(home => {return homeGoals.Year === 2014 && home.Stage === 'Final'})[0]["Home Team Name"]);
+
+/*(e) Winner of 2014 world cup final */
 
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
-
-    /* code here */
+function getFinals(array) {
+    return fifaData.filter(array => array.Stage === "Final");
+    
 
 };
+console.log(getFinals(fifaData));
+
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
-
-    /* code here */
+function getYears(callback) {
+    const years = callback.map(years => {
+        return {'Year': year.Year};
+    });
+    return years;
 
 };
 
-getYears();
+
+
+getYears(getFinals(fifaData));
 
 /* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
